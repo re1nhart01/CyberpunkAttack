16aef610c8ff26b7c20a1b2c8cd48ee6f85f65a1
Evgeniy Kokaiko <70027656+re1nhart01@users.noreply.github.com>
2023-10-27 05:36:37 +0300

    1234

--- packages/linker/src/components/PunkCarousel/PunkCarousel.js
+++ packages/linker/src/components/PunkCarousel/PunkCarousel.js
@@ -5,12 +5,12 @@ import EllipseView from "../EllipseView/EllipseView";
 
 const PunkCarousel = () => {
   return (
-    <>
+    <div className="w-[100vw] relative overflow-x-visible">
     <EllipseView />
       <div className="relative h-[300px] w-full flex flex-row justify-center">
         <img style={{ top: 0, height: 360, width: 277, bottom: -50 }} className="absolute" src={Images.GlitchPunk4} alt="" />
       </div>
-    </>
+    </div>
   );
 };
 
