1b06c8a2554b665fd097bf0a3c82d6ad92aee123
Evgeniy Kokaiko <70027656+re1nhart01@users.noreply.github.com>
2024-09-23 22:14:43 +0300

    12

--- packages/qrcode/main.go
+++ packages/qrcode/main.go
@@ -3,7 +3,7 @@ package main
 import qrcode "github.com/skip2/go-qrcode"
 
 func main() {
-	err := qrcode.WriteFile("https://cyberpunk-attack-linker.web.app/", qrcode.Medium, 1024, "qr.png")
+	err := qrcode.WriteFile("https://redirect.cyberpunkattack.xyz/?redirect=nJ6L9fg6hEDIey5pJHSwg97r0gGWvABv", qrcode.Medium, 1024, "qr.png")
 	if err != nil {
 		panic(err)
 	}
